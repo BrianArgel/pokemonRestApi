370393a9f6c707d77c4912dab05c2b66e6e7aff4
bponce <bponce@estrategiasdocumentales.com>
2023-09-18 21:27:36 -0500

    feat: add .env PORT

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
+import dotenv from 'dotenv'
 import react from "@vitejs/plugin-react";
 import path from "path";
+
+dotenv.config() // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.VITE_REACT_APP_PORT) || 3010,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
